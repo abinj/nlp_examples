2c733a280c81a532927f1e078c31358c8131e416
abinj <abinmj656@gmail.com>
2020-07-11 19:08:12 -0400

    Newly trained NER model tested succesfully

--- stanford_nlp.py
+++ stanford_nlp.py
@@ -1,31 +1,31 @@
 import nltk
 import corenlp
 
-# nltk.download('punkt')
-from nltk import StanfordNERTagger, word_tokenize
+nltk.download('punkt')
+from nltk import StanfordNERTagger
 
 
 
 # NER Using NLTK
-# st = StanfordNERTagger('/home/abin/my_works/nlp/stanford-ner-4.0.0/classifiers/english.all.3class.distsim.crf.ser.gz'
-#                        , '/home/abin/my_works/nlp/stanford-ner-4.0.0/stanford-ner.jar', encoding='utf-8')
+st = StanfordNERTagger('/home/abin/my_works/nlp/stanford-ner-4.0.0/ner-model.ser.gz'
+                       , '/home/abin/my_works/nlp/stanford-ner-4.0.0/stanford-ner.jar', encoding='utf-8')
 #
-text = 'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Dexlock.'
+text = 'Number of glucocorticoid receptors in lymphocytes and their sensitivity to hormone action.'
 #
-# tokenized_text = word_tokenize(text)
-# classified_text = st.tag(tokenized_text)
-#
-# print(classified_text)
+tokenized_text = nltk.word_tokenize(text)
+classified_text = st.tag(tokenized_text)
+
+print(classified_text)
 
 
 
 # NER using stanford-corenlp library
 # Make sure you have set $CORENLP_HOME as environment variable before start to use Stanford CoreNLPClient
 
-with corenlp.CoreNLPClient(annotators="ner".split(), memory='2G') as client:
-  ann = client.annotate(text)
+# with corenlp.CoreNLPClient(annotators="ner".split(), memory='2G') as client:
+#   ann = client.annotate(text)
 
-print(ann)
+# print(ann)
 
 
 
